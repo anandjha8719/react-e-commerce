58e2aa39ed16367ed18774c81b9e70eb20f2790a
anandjha8719 <anandjha8719@gmail.com>
2022-08-06 09:49:46 +0530

    user auth, create user or check if already exist

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
   GoogleAuthProvider,
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
+
 const firebaseConfig = {
   apiKey: "AIzaSyB-bQqXRe9t7PFbTnf6fcBjV6ctvM7DhHw",
   authDomain: "shopping-db-88caf.firebaseapp.com",
@@ -23,11 +30,37 @@ provider.setCustomParameters({
   prompt: 'select_account',
 });
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
+// export const createUserProfileDocument = async (userAuth, additionalData) => {
+//   if (!userAuth) return;
 
-  console.log(userAuth);
-};
+//   console.log(userAuth);
+// };
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapsot = await getDoc(userDocRef);
+
+  //if user dne
+  if(!userSnapsot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date;
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error in creating new user:', error.message);
+    }
+  }
+  //if user exist
+  return userDocRef;
+};

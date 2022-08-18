39009b4f6d76058ebd6399418f49903ae9e28e6d
anandjha8719 <anandjha8719@gmail.com>
2022-08-18 17:31:27 +0530

    root-reducer and diffrent reducers

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
 import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
-import { CartProvider } from './contexts/cart.context';
+import { store } from './store/store';
 
 import './index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
 
-root.render(
+render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  rootElement
 );
